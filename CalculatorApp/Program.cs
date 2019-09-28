f8261d1df7a3c60a820bc5d1e487707f37346662
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-09-28 12:15:37 +0200

    Creazione di un progetto in console che permette di salutare in italiano e in inglese.

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("Ciao Mondo");
+            Console.ReadLine();
+        }
+    }
+}
